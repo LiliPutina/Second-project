fcc2b3ced2fd5ac5671197a57a1e46b2719f45a2
Лилия Идиятуллина <Lily_idylle@MacBook-Air-Fy.local>
2017-02-17 16:00:35 +0300

    Пробуем гит

---
+++ src/main/java/com/alfa/Main.java
@@ -0,0 +1,14 @@
+package com.alfa;
+
+/**
+ * Created by Lily_idylle on 14.02.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String somebody= "bitch";
+        System.out.println("Boom, " + somebody);
+        double x=7;
+        double y=x*x;
+        System.out.println("Площадь квадрата со стороной " + x + " = " + (y));
+    }
+}
